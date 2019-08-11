6852b525699f6c6e3921b6e6eb8cae7d3f72e77b
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-11 15:32:29 -0600

    Add snapshot test with react renderer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Messages from "./Messages";
 function App() {
   return (
     <div className="App">
-      <h1>YOUR APPLICATION NAME!</h1>
+      <h1>Message Master!</h1>
       <div>Hello!!!!</div>
       <Messages name="Messages" unread={0} />
       <Messages name="Notifications" unread={10} />
