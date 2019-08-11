b9858e8b72638add08fb5ceed69c6958ce958fd4
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-11 15:40:29 -0600

    Add snapshot test for messages=0

--- src/Messages.test.js
+++ src/Messages.test.js
@@ -10,9 +10,17 @@ it("renders without crashing", () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
+// Snap shot tests:
 it("renders the UI as expected", () => {
   const tree = renderer
     .create(<Messages name="Messages" unread={4} />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("renders the UI as expected with no unreads", () => {
+  const tree = renderer
+    .create(<Messages name="Messages" unread={0} />)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
