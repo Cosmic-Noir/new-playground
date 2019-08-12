e834253b11e9accc89a34618c62cc8f512fb1cda
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-11 19:06:47 -0600

    Add button with displayed counter, add onClick functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Messages from "./Messages";
 import TheDate from "./state/TheDate";
+import Counter from "./state/Counter";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Messages name="Messages" unread={0} />
       <Messages name="Notifications" unread={10} />
       <TheDate />
+      <Counter />
     </div>
   );
 }
