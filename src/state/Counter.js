abd77dcb078788b26ce8743db2aca1f50735b898
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-11 19:15:25 -0600

    Add constructor method to bind handleButtonClick function to this

--- src/state/Counter.js
+++ src/state/Counter.js
@@ -1,20 +1,21 @@
 import React from "react";
 
 class Counter extends React.Component {
-  state = {
-    count: 0
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+  handleButtonClick() {
+    console.log(this.state.count);
+  }
   render() {
     return (
       <div>
         <p>The current count: {this.state.count}</p>
-        <button
-          onClick={function() {
-            console.log("clicked!");
-          }}
-        >
-          Add 1
-        </button>
+        <button onClick={this.handleButtonClick}>Add 1</button>
       </div>
     );
   }
