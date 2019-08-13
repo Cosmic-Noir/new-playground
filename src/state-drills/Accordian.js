c4122f8a60be634b8d4de7ec5109d124ce057ab6
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-13 16:49:19 -0600

    Add Accordian component

---
+++ src/state-drills/Accordian.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Accordian extends React.Component {
+  static defaultProps = {
+    tabs: []
+  };
+
+  render() {
+    return <div />;
+  }
+}
+
+export default Accordian;
