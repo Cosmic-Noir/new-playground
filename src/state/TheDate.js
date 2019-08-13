47a865801728c548a1159b87e93195c8ffd4a75b
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-12 20:15:31 -0600

    Added Tabs component with test.js file

--- src/state/TheDate.js
+++ src/state/TheDate.js
@@ -7,12 +7,12 @@ class TheDate extends React.Component {
       datetime: new Date(),
       step: 1
     };
-    console.log("constructor");
+    // console.log("constructor");
   }
   componentDidMount() {
-    console.log("componentDidMount");
+    // console.log("componentDidMount");
     this.interval = setInterval(() => {
-      console.log("setInterval");
+      // console.log("setInterval");
       this.setState({ datetime: new Date() });
     }, 1000);
   }
@@ -21,7 +21,7 @@ class TheDate extends React.Component {
     clearInterval(this.interval);
   }
   render() {
-    console.log("render");
+    // console.log("render");
     return <div>{this.state.datetime.toLocaleString()}</div>;
   }
 }
