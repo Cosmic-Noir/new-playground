2185a420391853c9d6c76598f45e0a2db2c14545
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-27 17:50:51 -0600

    Add promise.js with aysnchronous examples

---
+++ promisePractice/promises.js
@@ -0,0 +1,21 @@
+// console.log("The code has started.");
+// const duration = Math.floor(Math.random() * 5000);
+// setTimeout(() => {
+//   console.log(`Hey! I waited ${duration} milliseconds!`);
+// }, duration);
+// console.log("This is the last statement in the file.");
+
+function doTask(name) {
+  console.log(`${name} has started`);
+  const duration = Math.floor(Math.random() * 5000);
+  setTimeout(() => {
+    console.log(`${name} has ended after ${duration} milliseconds`);
+  }, duration);
+}
+
+doTask("A");
+doTask("B");
+doTask("C");
+doTask("D");
+doTask("E");
+doTask("F");
