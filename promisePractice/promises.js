34066e1465cc000e6db83ddadd42fbde3f434013
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-27 18:14:38 -0600

    Add other tasks to test resolve times

--- promisePractice/promises.js
+++ promisePractice/promises.js
@@ -63,20 +63,58 @@
 //   console.log("The promise has failed with the following message:");
 //   console.log(err);
 // });
-const p1 = new Promise((resolve, reject) => {
-  console.log("Running the asynchronous code here");
-  const duration = Math.floor(Math.random() * 5000);
-  setTimeout(() => {
-    console.log("About to resolve");
-    resolve(42); //succeed with a value
-  }, duration);
+
+// const p1 = new Promise((resolve, reject) => {
+//   console.log("Running the asynchronous code here");
+//   const duration = Math.floor(Math.random() * 5000);
+//   setTimeout(() => {
+//     console.log("About to resolve");
+//     resolve(42); //succeed with a value
+//   }, duration);
+// });
+
+// p1.then(value => {
+//   //notice the value parameter here
+//   console.log("The promise completed successfully with the following value:");
+//   console.log(value);
+// }).catch(err => {
+//   console.log("The promise has failed with the following message:");
+//   console.log(err);
+// });
+
+function doTask(name) {
+  const p = new Promise((resolve, reject) => {
+    console.log(`${name} has started`);
+    const duration = Math.floor(Math.random() * 5000);
+    setTimeout(() => {
+      resolve(`${name} has ended after ${duration} milliseconds`);
+    }, duration);
+  });
+  return p;
+}
+
+// const p2 = doTask("A");
+// console.log(p2);
+
+doTask("A").then(result => {
+  console.log(result);
+});
+doTask("B").then(result => {
+  console.log(result);
 });
 
-p1.then(value => {
-  //notice the value parameter here
-  console.log("The promise completed successfully with the following value:");
-  console.log(value);
-}).catch(err => {
-  console.log("The promise has failed with the following message:");
-  console.log(err);
+doTask("C").then(result => {
+  console.log(result);
+});
+
+doTask("D").then(result => {
+  console.log(result);
+});
+
+doTask("E").then(result => {
+  console.log(result);
+});
+
+doTask("F").then(result => {
+  console.log(result);
 });
