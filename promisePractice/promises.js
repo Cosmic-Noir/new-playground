126b6fe0333d67c49c2dcb318a6b898da2720169
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-27 18:08:43 -0600

    Test passing values to resolve and reject

--- promisePractice/promises.js
+++ promisePractice/promises.js
@@ -33,17 +33,50 @@
 //   console.log("The promise completed sucessfully");
 // });
 
+// const p1 = new Promise((resolve, reject) => {
+//   console.log("Running the asynchronous code here");
+//   const duration = Math.floor(Math.random() * 5000);
+//   setTimeout(() => {
+//     console.log("About to fail");
+//     reject(); //fail!
+//   }, duration);
+// });
+
+// p1.then(() => {
+//   console.log("The promise completed successfully");
+// }).catch(() => {
+//   console.log("The promise has failed.");
+// });
+// const p1 = new Promise((resolve, reject) => {
+//   console.log("Running the asynchronous code here");
+//   const duration = Math.floor(Math.random() * 5000);
+//   setTimeout(() => {
+//     console.log("About to fail");
+//     reject("Error 42: Life has no meaning"); // fail with a message
+//   }, duration);
+// });
+
+// p1.then(() => {
+//   console.log("The promise completed successfully");
+// }).catch(err => {
+//   //notice the err parameter here
+//   console.log("The promise has failed with the following message:");
+//   console.log(err);
+// });
 const p1 = new Promise((resolve, reject) => {
   console.log("Running the asynchronous code here");
   const duration = Math.floor(Math.random() * 5000);
   setTimeout(() => {
-    console.log("About to fail");
-    reject(); //fail!
+    console.log("About to resolve");
+    resolve(42); //succeed with a value
   }, duration);
 });
 
-p1.then(() => {
-  console.log("The promise completed successfully");
-}).catch(() => {
-  console.log("The promise has failed.");
+p1.then(value => {
+  //notice the value parameter here
+  console.log("The promise completed successfully with the following value:");
+  console.log(value);
+}).catch(err => {
+  console.log("The promise has failed with the following message:");
+  console.log(err);
 });
