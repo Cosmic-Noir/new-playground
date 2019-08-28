03969730356d3e0e6fad6537d6865731184da70e
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-27 18:00:12 -0600

    Add new promises testing resolve and reject

--- promisePractice/promises.js
+++ promisePractice/promises.js
@@ -5,17 +5,45 @@
 // }, duration);
 // console.log("This is the last statement in the file.");
 
-function doTask(name) {
-  console.log(`${name} has started`);
+// function doTask(name) {
+//   console.log(`${name} has started`);
+//   const duration = Math.floor(Math.random() * 5000);
+//   setTimeout(() => {
+//     console.log(`${name} has ended after ${duration} milliseconds`);
+//   }, duration);
+// }
+
+// doTask("A");
+// doTask("B");
+// doTask("C");
+// doTask("D");
+// doTask("E");
+// doTask("F");
+
+// const p1 = new Promise((resolve, reject) => {
+//   console.log("Running the asynchronous code here");
+//   const duration = Math.floor(Math.random() * 5000);
+//   setTimeout(() => {
+//     console.log("About to resolve");
+//     resolve(); // success
+//   }, duration);
+// });
+
+// p1.then(() => {
+//   console.log("The promise completed sucessfully");
+// });
+
+const p1 = new Promise((resolve, reject) => {
+  console.log("Running the asynchronous code here");
   const duration = Math.floor(Math.random() * 5000);
   setTimeout(() => {
-    console.log(`${name} has ended after ${duration} milliseconds`);
+    console.log("About to fail");
+    reject(); //fail!
   }, duration);
-}
+});
 
-doTask("A");
-doTask("B");
-doTask("C");
-doTask("D");
-doTask("E");
-doTask("F");
+p1.then(() => {
+  console.log("The promise completed successfully");
+}).catch(() => {
+  console.log("The promise has failed.");
+});
