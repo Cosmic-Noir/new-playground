1819aa0d34b1bbfa5a136dce92301d6620b6ebba
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-31 16:30:38 -0600

    Update promiseNotes.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import HelloWorld from "./state-drills/HelloWorld";
 import Bomb from "./state-drills/Bomb";
 import RouletteGun from "./state-drills/RouletteGun";
 import Tabs from "./state/Tabs";
+import Accordian from "./state-drills/Accordian";
 
 const tabsProp = [
   {
@@ -26,6 +27,23 @@ const tabsProp = [
   }
 ];
 
+const sections = [
+  {
+    title: "Section 1",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+  },
+  {
+    title: "Section 2",
+    content:
+      "Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!"
+  },
+  {
+    title: "Section 3",
+    content:
+      "Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?"
+  }
+];
+
 function App() {
   return (
     <div className="App">
@@ -39,6 +57,7 @@ function App() {
       <Bomb />
       <RouletteGun />
       <Tabs tabs={tabsProp} />
+      <Accordian sections={sections} />
     </div>
   );
 }
---
+++ src/promiseNotes.js
@@ -0,0 +1,4 @@
+// Example for notes
+setTimeout(() => {
+    console.log('Hey, I waited 3 seconds.');
+}, 3000);)
