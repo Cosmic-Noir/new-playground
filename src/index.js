6ff3112fc7596bdb1520f8ab472435e8d206da78
Kristen Doucette <cosmicnoir90@gmail.com>
2019-09-03 18:15:24 -0600

    Add RegistrationForm component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./paltrows-power-toes/src/App";
 import AppLang from "./lang-context/AppLang";
+import RegistrationForm from "./registrationForm/registrationForm";
 
 // import DemonymApp from "./demonymapp/demonymApp";
 
 ReactDOM.render(
   <BrowserRouter>
-    <AppLang />
+    <RegistrationForm />
   </BrowserRouter>,
   document.getElementById("root")
 );
