4c61070592b73b2ca899d0336d16110bfa8c058a
Kristen Doucette <cosmicnoir90@gmail.com>
2019-09-03 20:02:32 -0600

    Add CurrencyError.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import App from "./paltrows-power-toes/src/App";
+// import App from "./paltrows-power-toes/src/App";
 import AppLang from "./lang-context/AppLang";
 import RegistrationForm from "./registrationForm/registrationForm";
+import App from "./playingwithdanger/App";
 
 // import DemonymApp from "./demonymapp/demonymApp";
 
 ReactDOM.render(
   <BrowserRouter>
-    <RegistrationForm />
+    <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
